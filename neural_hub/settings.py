aef5e179243721abc4a99e1233c3be38cc20f805
deso07 <valentinnikotin2006@gmail.com>
2025-05-06 18:44:21 +0000

    Update static files and fix project structure

--- neural_hub/settings.py
+++ neural_hub/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,10 +116,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/weeu/startup/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/weeu/startup/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'orders.User'
 
